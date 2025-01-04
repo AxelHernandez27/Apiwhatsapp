30b19c04223760266c7cb7379d4913016e5d498c
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2025-01-03 21:38:25 -0600

    variable number

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,88 +1,146 @@
 const https = require("https");
 
-function EnviarMensajeWhatssApp(text, number) {
-    // Asegúrate de que el número esté en formato internacional.
+function EnviarMensajeWhatssApp(text, number){
+
     if (!/^\d+$/.test(number)) {
         console.error("Número inválido:", number);
         return;
     } else {
-        console.log("Número válido: ", number);
+        console.error("Número válido:", number);
     }
 
-    // Convertir texto a minúsculas.
     text = text.toLowerCase();
+    
+    if(text.includes("hola")){
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "Hola, bienvenida a Edith Manrique MakeUp Artist.😊"
+            }
+    
+        });
+    }else if(text =="1"){
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "¡Aquí tienes nuestra lista de servicios!\n\n📋 **Maquillaje**:\n- Maquillaje social (día o noche)\n- Maquillaje nupcial\n- Maquillaje XV años\n- Maquillaje piel madura\n- Maquillaje editorial\n- Maquillaje full color\n- Maquillaje con delineados gráficos\n\n📋 **Peinados**:\n- Peinado con Ondas de agua y Hollywood\n- Peinado alaciado\n- Peinados sueltos\n- Peinados semirecogidos\n\n✍️ Escribe el servicio que deseas y con gusto te ayudaré a agendarlo. 😊"
 
-    // Respuestas automáticas.
-    let respuesta = "";
-    switch (true) {
-        case text.includes("hola"):
-            respuesta = "Hola, bienvenida a Edith Manrique MakeUp Artist.😊";
-            break;
-        case text === "1":
-            respuesta = "¡Aquí tienes nuestra lista de servicios! ... (tu mensaje aquí)";
-            break;
-        case text === "2":
-            respuesta = "Nuestros días de atención son de lunes a domingo ...";
-            break;
-        case text === "3":
-            respuesta = "🌟 Para agendar tu cita, proporciona los siguientes datos ...";
-            break;
-        case text === "4":
-            respuesta = "✨ Para conocer más sobre nuestro trabajo, visita nuestra página ...";
-            break;
-        case text === "5":
-            respuesta = "🙏 Gracias por contactarnos, en un momento nuestra asesora te contestará.";
-            break;
-        case text === "0":
-            respuesta = "Hola🌟\n\n¿Ya tuviste oportunidad de visitar nuestra página web? ...";
-            break;
-        default:
-            respuesta = "Hola🌟\n\n¿Ya tuviste oportunidad de visitar nuestra página web? ...";
-            break;
+            }
+    
+        });
+    
+    }else if (text =="2"){
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "Nuestros días de atención son de lunes a domingo en el horario que más se acomode a tu evento (a disponibilidad). 😊"
+            }
+        });
+    }else if (text == "3"){
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "🌟 Para agendar tu cita, proporciona los siguientes datos para revisar nuestra disponibilidad:\n\n📝 **Datos necesarios**:\n- Nombre:\n- Día de cita:\n- Hora de cita:\n- Dirección del servicio:\n\n⏳ *Se te notificará cuando tu cita esté agendada.*\n📅 En caso de no tener disponibilidad, te ofreceremos horarios alternativos para que elijas el que mejor se adapte a tus tiempos. 😊"
+
+
+
+            }
+    
+        });
+    }else if (text =="4"){
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "✨ Para conocer más sobre nuestro trabajo, visita nuestra página:\n🌐 [Portafolio](https://edithmanriquemakeupartist.netlify.app/portafolio)\n\n📲 **Síguenos en nuestras redes sociales para más contenido reciente:**\n- Instagram: [edithmanriquemakeup](https://www.instagram.com/edithmanriquemakeup?igsh=bzg1eTFyN2w5ZDNk)\n- Facebook: [Edith Manrique Makeup Artist](https://www.facebook.com/profile.php?id=61568716406850&mibextid=ZbWKwL)\n\n💖 ¡Gracias por tu apoyo! #MakeupLover"
+
+
+
+
+            }
+    
+        });
+    }else if (text =="5"){
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "🙏 Gracias por contactarnos, en un momento uno de nuestra asesora te contestará.💬\n\n¡Gracias por tu paciencia! 😊"
+            }
+    
+        });
+    }else if (text == "0"){
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "Hola🌟\n\n¿Ya tuviste oportunidad de visitar nuestra página web? 👉 edithmanriquemakeupartist.netlify.app/inicio\nEn ella encontrarás información sobre nuestros servicios de peinados y maquillaje.\n\n📋 Selecciona una opción para continuar:\n1️⃣ Información sobre servicios de maquillaje y peinados.\n2️⃣ Conocer nuestros horarios de atención.\n3️⃣ Reservar una cita.\n4️⃣ Ver nuestros trabajos recientes.\n5️⃣ Hablar con un asesor.\n0️⃣ Regresar al menú\n\nEscribe el número de la opción que deseas y con gusto te ayudaré. 😊"
+
+            }
+    
+        });
+    }else{
+        var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to" : number,
+            "type": "text",
+            "text" : {
+                "preview_url" : false,
+                "body": "Hola🌟\n\n¿Ya tuviste oportunidad de visitar nuestra página web? 👉 edithmanriquemakeupartist.netlify.app/inicio\nEn ella encontrarás información sobre nuestros servicios de peinados y maquillaje.\n\n📋 Selecciona una opción para continuar:\n1️⃣ Información sobre servicios de maquillaje y peinados.\n2️⃣ Conocer nuestros horarios de atención.\n3️⃣ Reservar una cita.\n4️⃣ Ver nuestros trabajos recientes.\n5️⃣ Hablar con un asesor.\n0️⃣ Regresar al menú\n\nEscribe el número de la opción que deseas y con gusto te ayudaré. 😊"
+
+            }
+    
+        });
     }
+  
 
-    // Datos para la API.
-    const data = JSON.stringify({
-        "messaging_product": "whatsapp",
-        "recipient_type": "individual",
-        "to": number,
-        "type": "text",
-        "text": {
-            "preview_url": false,
-            "body": respuesta,
-        },
-    });
-
-    // Configuración de la solicitud HTTPS.
-    const options = {
+    const options ={
         host: "graph.facebook.com",
         path: "/v21.0/503111629557940/messages",
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer EAAPkn3wIwJ8BO6PZCm09apLLwayBzUU26YcGi54S4xp02dNeFvBGRZCR0qylfKK9tC2lW5jdq77XPN0ri8wqf0gnklfHYNVvjjEAemmv6NmRMtODgv7OTPZAj8mzHpSZA3e80F7pPlJGjilfCTha8VZCHqgZCJIr2aTXbi994mP7Ea3yzo0vhYUOuVv7UkDJOz",
-        },
+        body: data,
+        headers : {
+            "Content-Type" : "application/json",
+            Authorization : "Bearer EAAPkn3wIwJ8BO6PZCm09apLLwayBzUU26YcGi54S4xp02dNeFvBGRZCR0qylfKK9tC2lW5jdq77XPN0ri8wqf0gnklfHYNVvjjEAemmv6NmRMtODgv7OTPZAj8mzHpSZA3e80F7pPlJGjilfCTha8VZCHqgZCJIr2aTXbi994mP7Ea3yzo0vhYUOuVv7UkDJOz"
+        }
     };
 
-    // Envío de la solicitud.
-    const req = https.request(options, (res) => {
-        let responseData = "";
-        res.on("data", (chunk) => {
-            responseData += chunk;
+    const req = https.request(options, res => {
+        res.on("data", d=> {
+            process.stdout.write(d);
         });
-        res.on("end", () => {
-            console.log("Respuesta de la API:", responseData);
-        });
-    });
-
-    req.on("error", (error) => {
-        console.error("Error al enviar mensaje:", error);
     });
 
     req.write(data);
     req.end();
 }
 
-module.exports = {
-    EnviarMensajeWhatssApp,
+module.exports= {
+    EnviarMensajeWhatssApp
 };
